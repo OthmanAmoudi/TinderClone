3f55f6701b7107cb1f97e0e58ebecad3b7950a87
Othman Mashaab <red.fantastic@hotmail.com>
2017-05-25 19:10:09 +0300

    added the FB framework with login/logout

--- TinderClone/ProfileViewController.swift
+++ TinderClone/ProfileViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+class ProfileViewController: UIViewController, FBSDKLoginButtonDelegate {
 
-class ProfileViewController: UIViewController {
-
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        var loginButton: FBSDKLoginButton = FBSDKLoginButton()
+        loginButton.delegate = self
+        view.addSubview(loginButton)
+        loginButton.frame = CGRect(x: 18, y: 630, width: view.frame.width - 32, height: 50)
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +28,24 @@ class ProfileViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                // Do work
+            }
+        }
     }
-    */
+    
+    
 
 }
--- TinderClone/ViewController.swift
+++ TinderClone/ViewController.swift
@@ -8,22 +8,51 @@
 
 import UIKit
 import Foundation
-class ViewController: UIViewController {
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
+class ViewController: UIViewController , FBSDKLoginButtonDelegate {
+    /**
+     Sent to the delegate when the button was used to logout.
+     - Parameter loginButton: The button that was clicked.
+     */
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        dismiss(animated: true, completion: nil)
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var loginButton: FBSDKLoginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        loginButton.delegate = self
+        view.addSubview(loginButton)
+        loginButton.frame = CGRect(x: 18, y: 650, width: view.frame.width - 32, height: 50)
+       
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error?) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        else{
+        goToProfile()
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func fbLogin_DidPressed(_ sender: Any) {
-        
+    
+    func goToProfile(){
         performSegue(withIdentifier: "ToHomeTabBarSegue", sender: nil)
     }
 
 }
+
 
