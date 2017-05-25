5855747879821dafef59609a9116b5626854b46e
Othman Mashaab <red.fantastic@hotmail.com>
2017-05-25 14:35:51 +0300

    set up the UI Navigation

--- TinderClone/ViewController.swift
+++ TinderClone/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Foundation
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func fbLogin_DidPressed(_ sender: Any) {
+        
+        performSegue(withIdentifier: "ToHomeTabBarSegue", sender: nil)
+    }
 
 }
 
